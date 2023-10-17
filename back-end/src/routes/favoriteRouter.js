50a67d986654ca91ccb504b73a2b4d6036917bb5
G-Alves1 <2kgabrielalves@gmail.com>
2023-10-17 20:23:53 -0300

    feat(routes): add favorite

--- back-end/src/routes/favoriteRouter.js
+++ back-end/src/routes/favoriteRouter.js
@@ -3,5 +3,6 @@ const favoriteController = require("../controllers/favoriteController");
 const { validToken } = require("../middlewares/createMiddleware");
 
 router.get("/", validToken, favoriteController.listFavorites);
+router.post("/", validToken, favoriteController.addFavorite);
 
 module.exports = router;
