e8959bcf2ee2f240ddf318b5d3f6a373f4fb7700
G-Alves1 <2kgabrielalves@gmail.com>
2023-10-17 20:21:56 -0300

    feat(services): add favorite

--- back-end/src/services/favoriteService.js
+++ back-end/src/services/favoriteService.js
@@ -1,13 +1,31 @@
 const { Favorite } = require("../database/models");
 
 const listFavorites = async (userId) => {
-  const favorites = await Favorite.findAll({
+  const favorites = await Favorite.findOne({
     where: { userId },
   });
 
   return favorites;
 };
 
+const addFavorite = async (userId, favoriteName) => {
+  const favorites = await Favorite.findOne({ where: { userId } });
+
+  if (!favorites) {
+    await Favorite.create({
+      userId,
+      favorite: [favoriteName],
+    });
+  } else {
+    if (!favorites.favorite.includes(favoriteName)) {
+      favorites.favorite = [...favorites.favorite, favoriteName];
+
+      await favorites.save();
+    }
+  }
+};
+
 module.exports = {
   listFavorites,
+  addFavorite,
 };
