73c555faf7e089797321243abe59dd15e0cf99e7
G-Alves1 <2kgabrielalves@gmail.com>
2023-10-17 20:53:54 -0300

    feat(routes): remove favorite

--- back-end/src/routes/favoriteRouter.js
+++ back-end/src/routes/favoriteRouter.js
@@ -4,5 +4,6 @@ const { validToken } = require("../middlewares/createMiddleware");
 
 router.get("/", validToken, favoriteController.listFavorites);
 router.post("/", validToken, favoriteController.addFavorite);
+router.delete("/", validToken, favoriteController.removeFavorite);
 
 module.exports = router;
