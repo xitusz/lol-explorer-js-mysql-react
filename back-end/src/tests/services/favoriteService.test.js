c7b930bc5b4c748f6c50338dcf213d77720be20d
G-Alves1 <2kgabrielalves@gmail.com>
2023-11-07 10:17:26 -0300

    test(services): addFavorites in favoriteService

--- back-end/src/tests/services/favoriteService.test.js
+++ back-end/src/tests/services/favoriteService.test.js
@@ -5,18 +5,16 @@ const favoriteService = require("../../services/favoriteService");
 const { Favorite } = require("../../database/models");
 
 describe("Favorite Service", () => {
+  afterEach(() => {
+    sinon.restore();
+  });
+
   describe("listFavorites", () => {
-    beforeEach(async () => {
+    it("should list the favorites", async () => {
       sinon.stub(Favorite, "findOne").resolves({
         favorite: ["Aatrox", "Ahri", "Akali"],
       });
-    });
 
-    afterEach(() => {
-      sinon.restore();
-    });
-
-    it("should list the favorites", async () => {
       const userId = 1;
 
       const favorites = await favoriteService.listFavorites(userId);
@@ -28,6 +26,33 @@ describe("Favorite Service", () => {
       expect(favorites).to.deep.equal({
         favorite: ["Aatrox", "Ahri", "Akali"],
       });
+    });
+  });
+
+  describe("addFavorite", () => {
+    it("should add a favorite", async () => {
+      const userId = 1;
+      const favoriteName = "Yasuo";
+
+      sinon.stub(Favorite, "findOne").resolves({
+        favorite: ["Aatrox", "Ahri", "Akali"],
+        save: sinon.stub(),
+      });
+
+      const result = await favoriteService.addFavorite(userId, favoriteName);
+      const favorites = await favoriteService.listFavorites(userId);
+
+      expect(Favorite.findOne.called).to.be.true;
+      expect(Favorite.findOne.firstCall.args[0]).to.deep.equal({
+        where: { userId },
+      });
+      expect(favorites.favorite).to.deep.equal([
+        "Aatrox",
+        "Ahri",
+        "Akali",
+        "Yasuo",
+      ]);
+      expect(result).to.be.undefined;
     });
   });
 });
