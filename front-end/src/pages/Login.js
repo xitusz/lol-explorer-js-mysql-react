300abf472c5d9461faabbeae885dc2d46f65673c
G-Alves1 <2kgabrielalves@gmail.com>
2023-12-15 13:58:06 -0300

    feat(pages): reCaptcha in Login

--- front-end/src/pages/Login.js
+++ front-end/src/pages/Login.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-undef */
 import React, { useState, useEffect } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import Button from "../components/Button";
@@ -9,11 +10,13 @@ import {
 import { AiOutlineMail } from "react-icons/ai";
 import { BsFillKeyFill } from "react-icons/bs";
 import axios from "axios";
+import ReCAPTCHA from "react-google-recaptcha";
 
 const Login = () => {
   const navigate = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [recaptchaValue, setRecaptchaValue] = useState(null);
   const [error, setError] = useState("");
 
   useEffect(() => {
@@ -24,17 +27,22 @@ const Login = () => {
   }, [navigate]);
 
   const handleLogin = async () => {
-    try {
-      await axios.post("http://localhost:3001/login", {
-        email,
-        password,
-      });
+    if (!recaptchaValue) {
+      setError("Captcha inválido");
+    } else {
+      try {
+        await axios.post("http://localhost:3001/login", {
+          email,
+          password,
+          recaptchaValue,
+        });
 
-      setItemToLocalStorage("isLoggedIn", true);
-      setItemToLocalStorage("user", [{ email }]);
-      navigate("/");
-    } catch (error) {
-      setError("Email ou senha inválida");
+        setItemToLocalStorage("isLoggedIn", true);
+        setItemToLocalStorage("user", [{ email }]);
+        navigate("/");
+      } catch (error) {
+        setError("Email ou senha inválida");
+      }
     }
   };
 
@@ -83,6 +91,12 @@ const Login = () => {
                     required
                   />
                 </div>
+              </div>
+              <div className="mb-2 d-flex justify-content-center">
+                <ReCAPTCHA
+                  sitekey={process.env.REACT_APP_RECAPTCHA_SITE_KEY}
+                  onChange={(value) => setRecaptchaValue(value)}
+                />
               </div>
               <Button
                 className="btn btn-primary w-100 mb-2"
