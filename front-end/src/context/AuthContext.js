178f839e9321fc9a203416fffdb9e2613f90e618
G-Alves1 <2kgabrielalves@gmail.com>
2023-12-21 18:26:26 -0300

    feat(context): AuthContext in the frontend

---
+++ front-end/src/context/AuthContext.js
@@ -0,0 +1,21 @@
+/* eslint-disable react/react-in-jsx-scope */
+import { createContext, useContext, useState } from "react";
+import PropTypes from "prop-types";
+
+const AuthContext = createContext();
+
+export const AuthProvider = ({ children }) => {
+  const [userToken, setUserToken] = useState("");
+
+  return (
+    <AuthContext.Provider value={{ userToken, setUserToken }}>
+      {children}
+    </AuthContext.Provider>
+  );
+};
+
+export const useAuth = () => useContext(AuthContext);
+
+AuthProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
