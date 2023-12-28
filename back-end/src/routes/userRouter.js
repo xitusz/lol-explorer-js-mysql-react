639438992f1f3e689d2ef168f626f751e95a3a9a
G-Alves1 <2kgabrielalves@gmail.com>
2023-12-28 03:37:10 -0300

    feat(routes): updateName in userRouter in the backend

--- back-end/src/routes/userRouter.js
+++ back-end/src/routes/userRouter.js
@@ -3,5 +3,6 @@ const userController = require("../controllers/userController");
 const { validToken } = require("../middlewares/createMiddleware");
 
 router.get("/", validToken, userController.getProfileInfo);
+router.put("/edit/name", validToken, userController.updateName);
 
 module.exports = router;
