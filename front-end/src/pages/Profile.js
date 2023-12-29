598ca293ec75b943cd8e40bfa0e4c0f1fb890b14
G-Alves1 <2kgabrielalves@gmail.com>
2023-12-29 09:32:25 -0300

    feat(pages): profile info in Profile in the frontend

--- front-end/src/pages/Profile.js
+++ front-end/src/pages/Profile.js
@@ -1,28 +1,74 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
 import Header from "../components/Header";
-import Loading from "../components/Loading";
+import Footer from "../components/Footer";
+import { getItemFromLocalStorage } from "../services/localStorage";
 import { Link } from "react-router-dom";
+import { FiMail } from "react-icons/fi";
+import { AiOutlineSetting } from "react-icons/ai";
+import axios from "axios";
+import { useAuth } from "../context/AuthContext";
 
 const Profile = () => {
+  const navigate = useNavigate();
+  const { userToken } = useAuth();
+
+  const [profileInfo, setProfileInfo] = useState({
+    name: "",
+    email: "",
+  });
+
+  useEffect(() => {
+    const isLoggedIn = getItemFromLocalStorage("isLoggedIn");
+    if (!isLoggedIn) {
+      navigate("/");
+    }
+
+    const loadUserProfile = async () => {
+      if (userToken) {
+        const response = await axios.get("http://localhost:3001/profile", {
+          headers: {
+            Authorization: userToken,
+          },
+        });
+
+        setProfileInfo({
+          name: response.data.name,
+          email: response.data.email,
+        });
+      }
+    };
+
+    loadUserProfile();
+  }, [navigate, userToken]);
+
   return (
     <div>
       <Header />
       <div className="py-5">
         <h1 className="text-center text-white py-5">Perfil</h1>
-        <h2 className="text-center text-white py-5">
-          Em construção <Loading />
-        </h2>
-        <h4 className="text-center text-white py-5">
-          Vá para -{">"}{" "}
-          <Link
-            to="/character"
-            className="text-white construction-character-link"
-          >
-            Personagens
-          </Link>{" "}
-          {"<"}-
-        </h4>
+        <div className="d-flex mb-5 w-50 justify-content-between m-auto border rounded py-3 profile-div">
+          <div className="px-3">
+            <h4 className="text-white text-center">
+              Bem Vindo, {profileInfo.name}
+            </h4>
+            <div className="d-flex">
+              <FiMail
+                size={25}
+                className="text-white"
+                data-testid="email-icon"
+              />
+              <p className="text-white mx-1">{profileInfo.email}</p>
+            </div>
+          </div>
+          <div className="px-3">
+            <Link to="/profile/edit">
+              <AiOutlineSetting size={30} className="text-white icon" />
+            </Link>
+          </div>
+        </div>
       </div>
+      <Footer />
     </div>
   );
 };
