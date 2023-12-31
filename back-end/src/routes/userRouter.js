a28bf7ecc2e7a5d180497a705ebeccfb205a82bb
G-Alves1 <2kgabrielalves@gmail.com>
2023-12-31 18:42:56 -0300

    feat(routes): deleteUser in userRouter in the backend

--- back-end/src/routes/userRouter.js
+++ back-end/src/routes/userRouter.js
@@ -6,5 +6,6 @@ router.get("/", validToken, userController.getProfileInfo);
 router.put("/edit/name", validToken, userController.updateName);
 router.put("/edit/email", validToken, userController.updateEmail);
 router.put("/edit/password", validToken, userController.updatePassword);
+router.delete("/", validToken, userController.deleteUser);
 
 module.exports = router;
