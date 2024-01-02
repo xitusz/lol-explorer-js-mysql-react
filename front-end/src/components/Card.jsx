fe39015782b9031600fbe117ce8d8d263c35fc1d
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-02 18:03:22 -0300

    style(css): remove h-100 in Card components in the frontend

--- front-end/src/components/Card.jsx
+++ front-end/src/components/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({ name, image }) => {
       <img
         src={image}
         alt={name}
-        className="card-img-top img-fluid h-100 card-image"
+        className="card-img-top img-fluid card-image"
       />
       <div className="card-body p-0">
         <h5 className="card-title text-center pt-1 text-white">{name}</h5>
