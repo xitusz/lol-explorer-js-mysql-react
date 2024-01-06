c15e307d51334996f331681ed7d7198cbce2dacd
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-06 12:16:58 -0300

    feat(utils): replace md5 with bcrypt in hash in the backend

--- back-end/src/utils/hash.js
+++ back-end/src/utils/hash.js
@@ -1,8 +1,17 @@
-const md5 = require("md5");
+const bcrypt = require("bcrypt");
 
-const hash = (str) => md5(str);
+const hash = async (str) => {
+  const saltRounds = 10;
+  const hashedPassword = await bcrypt.hash(str, saltRounds);
 
-const verify = (str, hashedPassword) => hashedPassword === hash(str);
+  return hashedPassword;
+};
+
+const verify = async (str, hashedPassword) => {
+  const isMatch = await bcrypt.compare(str, hashedPassword);
+
+  return isMatch;
+};
 
 module.exports = {
   hash,
