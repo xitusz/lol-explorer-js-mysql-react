2e1c6e61a2d41fcf3d9311691306b43111eadce6
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-09 13:45:57 -0300

    refactor(middleware): remove console.log and change token validation message in createMiddleware in the backend

--- back-end/src/middlewares/createMiddleware.js
+++ back-end/src/middlewares/createMiddleware.js
@@ -6,7 +6,6 @@ const validEmail = (req, res, next) => {
   const regex = /^[a-z0-9-_.]+@[a-z0-9]+\.[a-z]+(\.[a-z]+)?$/gi;
 
   if (!regex.test(email)) {
-    console.log("Insira um email válido");
     return res.status(401).json({ message: "Insira um email válido" });
   }
 
@@ -17,7 +16,6 @@ const validPassword = (req, res, next) => {
   const { password } = req.body;
 
   if (password.length < 6 || password.length > 12) {
-    console.log("A senha deve ter de 6 a 12 caracteres");
     return res
       .status(401)
       .json({ message: "A senha deve ter de 6 a 12 caracteres" });
@@ -30,7 +28,6 @@ const validName = (req, res, next) => {
   const { name } = req.body;
 
   if (name.length < 2) {
-    console.log("O nome deve ter pelo menos 2 caracteres");
     return res
       .status(401)
       .json({ message: "O nome deve ter pelo menos 2 caracteres" });
@@ -44,11 +41,11 @@ const validToken = (req, res, next) => {
   const token = verifyToken(authorization);
 
   if (!authorization) {
-    return res.status(401).json({ message: "Token not found" });
+    return res.status(401).json({ message: "Token não encontrado" });
   }
 
   if (!token) {
-    res.status(401).json({ message: "Expired or invalid token" });
+    res.status(401).json({ message: "Token expirado ou inválido" });
   }
 
   req.user = token;
