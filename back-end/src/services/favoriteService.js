bf8983f99ab2dce754a0e8458d205c7b005c4270
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-09 00:22:15 -0300

    feat(services): error handling in favoriteService in the backend

--- back-end/src/services/favoriteService.js
+++ back-end/src/services/favoriteService.js
@@ -1,62 +1,90 @@
 const { Favorite } = require("../database/models");
 
 const createFavorites = async (userId) => {
-  const favorites = await Favorite.findOne({
-    where: { userId },
-  });
-
-  if (!favorites) {
-    await Favorite.create({
-      userId,
-      favorite: [],
+  try {
+    const favorites = await Favorite.findOne({
+      where: { userId },
     });
+
+    if (!favorites) {
+      await Favorite.create({
+        userId,
+        favorite: [],
+      });
+    }
+
+    return "Favorito criado com sucesso";
+  } catch (err) {
+    throw new Error(`Erro ao criar favorito: ${err}`);
   }
 };
 
 const listFavorites = async (userId) => {
-  const favorites = await Favorite.findOne({
-    where: { userId },
-  });
+  try {
+    const favorites = await Favorite.findOne({
+      where: { userId },
+    });
 
-  return favorites;
+    return favorites;
+  } catch (err) {
+    throw new Error(`Erro ao listar favoritos: ${err}`);
+  }
 };
 
 const addFavorite = async (userId, favoriteName) => {
-  const favorites = await Favorite.findOne({ where: { userId } });
+  try {
+    const favorites = await Favorite.findOne({ where: { userId } });
 
-  if (!favorites) {
-    await Favorite.create({
-      userId,
-      favorite: [favoriteName],
-    });
-  } else {
-    if (!favorites.favorite.includes(favoriteName)) {
-      favorites.favorite = [...favorites.favorite, favoriteName];
+    if (!favorites) {
+      await Favorite.create({
+        userId,
+        favorite: [favoriteName],
+      });
+    } else {
+      if (!favorites.favorite.includes(favoriteName)) {
+        favorites.favorite = [...favorites.favorite, favoriteName];
 
-      await favorites.save();
+        await favorites.save();
+      }
     }
+
+    return "Favorito adicionado com sucesso";
+  } catch (err) {
+    throw new Error(`Erro ao adicionar favorito: ${err}`);
   }
 };
 
 const removeFavorite = async (userId, favoriteName) => {
-  const favorites = await Favorite.findOne({ where: { userId } });
+  try {
+    const favorites = await Favorite.findOne({ where: { userId } });
 
-  if (favorites) {
-    favorites.favorite = favorites.favorite.filter(
-      (name) => name !== favoriteName
-    );
+    if (favorites) {
+      favorites.favorite = favorites.favorite.filter(
+        (name) => name !== favoriteName
+      );
 
-    await favorites.save();
+      await favorites.save();
+    }
+
+    return "Favorito removido com sucesso";
+  } catch (err) {
+    throw new Error(`Erro ao remover favorito: ${err}`);
   }
 };
 
 const clearFavorites = async (userId) => {
-  const favorites = await Favorite.findOne({ where: { userId } });
+  try {
+    const favorites = await Favorite.findOne({ where: { userId } });
 
-  if (favorites) {
-    favorites.favorite = [];
+    if (favorites) {
+      favorites.favorite = [];
 
-    await favorites.save();
+      await favorites.save();
+    }
+
+    return "Todos os favoritos foram removidos com sucesso";
+  } catch (err) {
+    throw new Error(`Erro ao limpar favoritos: ${err}`);
   }
 };
 
