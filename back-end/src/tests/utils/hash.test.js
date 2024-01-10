89fecce00910e7e6670406694db0cc9d7ee06fea
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-10 17:16:31 -0300

    test(utils): verify in hash in the backend

--- back-end/src/tests/utils/hash.test.js
+++ back-end/src/tests/utils/hash.test.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 const { expect } = require("chai");
-const { hash } = require("../../utils/hash");
+const { hash, verify } = require("../../utils/hash");
 const sinon = require("sinon");
 const bcrypt = require("bcrypt");
 
@@ -26,6 +26,43 @@ describe("Hash Util", () => {
         await hash("123456");
       } catch (err) {
         expect(err.message).to.equal("Erro ao fazer o hashing da senha: Error");
+      }
+    });
+  });
+
+  describe("verify", () => {
+    it("should return true for a valid password", async () => {
+      const bcryptStub = sinon.stub(bcrypt, "compare").resolves(true);
+
+      const password = "123456";
+      const hashedPassword = "hashedPassword";
+      const isMatch = await verify(password, hashedPassword);
+
+      expect(bcryptStub.calledWith(password, hashedPassword)).to.be.true;
+      expect(isMatch).to.be.true;
+    });
+
+    it("should return false for an invalid password", async () => {
+      const bcryptStub = sinon.stub(bcrypt, "compare").resolves(false);
+
+      const password = "invalid";
+      const hashedPassword = "hashedPassword";
+      const isMatch = await verify(password, hashedPassword);
+
+      expect(bcryptStub.calledWith(password, hashedPassword)).to.be.true;
+      expect(isMatch).to.be.false;
+    });
+
+    it("should handle error verifying password", async () => {
+      sinon.stub(bcrypt, "compare").rejects(new Error());
+
+      const password = "123456";
+      const hashedPassword = "hashedPassword";
+
+      try {
+        await verify(password, hashedPassword);
+      } catch (err) {
+        expect(err.message).to.equal("Erro ao verificar a senha: Error");
       }
     });
   });
