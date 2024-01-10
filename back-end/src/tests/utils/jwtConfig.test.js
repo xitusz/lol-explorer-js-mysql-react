46f5a38ea617bec206eba65c337e8e6e6397f898
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-10 16:55:36 -0300

    test(utils): sign in jwtConfig in the backend

--- back-end/src/tests/utils/jwtConfig.test.js
+++ back-end/src/tests/utils/jwtConfig.test.js
@@ -1,15 +1,35 @@
 /* eslint-disable no-undef */
 const { expect } = require("chai");
-const { sign, verifyToken } = require("../../utils/jwtConfig");
+const sinon = require("sinon");
+const jwt = require("jsonwebtoken");
+const { sign } = require("../../utils/jwtConfig");
 
 describe("JWT Util", () => {
-  it("should sign and verify a token", () => {
-    const payload = { id: 1, name: "user", email: "user1@example.com" };
-    const token = sign(payload);
-    const decoded = verifyToken(token);
+  const payload = { id: 1, name: "user", email: "user@example.com" };
 
-    expect(decoded).to.have.property("id").to.equal(1);
-    expect(decoded).to.have.property("name").to.equal("user");
-    expect(decoded).to.have.property("email").to.equal("user1@example.com");
+  afterEach(() => {
+    sinon.restore();
+  });
+
+  describe("sign", () => {
+    it("should sign a token", () => {
+      const signStub = sinon.stub(jwt, "sign").returns("token");
+
+      const token = sign(payload);
+
+      expect(signStub.calledWith(payload, sinon.match.any, sinon.match.any)).to
+        .be.true;
+      expect(token).to.equal("token");
+    });
+
+    it("should handle error sign token", () => {
+      sinon.stub(jwt, "sign").throws(new Error());
+
+      try {
+        sign(payload);
+      } catch (err) {
+        expect(err.message).to.equal(`Erro ao assinar o token JWT: Error`);
+      }
+    });
   });
 });
