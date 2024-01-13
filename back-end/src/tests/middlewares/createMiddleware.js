a963d7e96b77ee168843af39b09ada7133417b30
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-13 11:45:02 -0300

    test(middleware): validName in createMiddleware in the backend

--- back-end/src/tests/middlewares/createMiddleware.js
+++ back-end/src/tests/middlewares/createMiddleware.js
@@ -3,6 +3,7 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const sinon = require("sinon");
 const app = require("../../../index");
+const axios = require("axios");
 
 const { expect } = chai;
 chai.use(chaiHttp);
@@ -30,6 +31,28 @@ describe("Create Middleware", () => {
 
       expect(response).to.have.status(401);
       expect(response.body.message).to.be.equal("Token não encontrado");
+    });
+  });
+
+  describe("validName", () => {
+    it("should handle error invalid name", async () => {
+      const name = "U";
+      const email = "user@email.com";
+      const password = "123456";
+      const recaptchaValue = "validRecaptchaValue";
+      const googleResponse = { data: { success: true } };
+
+      sinon.stub(axios, "post").resolves(googleResponse);
+
+      const response = await chai
+        .request(app)
+        .post("/register")
+        .send({ name, email, password, recaptchaValue });
+
+      expect(response).to.have.status(401);
+      expect(response.body.message).to.equal(
+        "O nome deve ter pelo menos 2 caracteres"
+      );
     });
   });
 });
