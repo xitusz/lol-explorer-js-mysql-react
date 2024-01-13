76fd5941a3bdc4eecc0dbd5c724093e2a02ac24d
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-13 10:39:43 -0300

    test(routes): post / in registerRouter in the backend

--- back-end/src/tests/routes/registerRouter.test.js
+++ back-end/src/tests/routes/registerRouter.test.js
@@ -3,80 +3,53 @@ const chai = require("chai");
 const chaiHttp = require("chai-http");
 const sinon = require("sinon");
 const app = require("../../../index");
-const { User } = require("../../database/models");
+const userService = require("../../services/userService");
+const axios = require("axios");
 
 const { expect } = chai;
 chai.use(chaiHttp);
 
 describe("Register Router", () => {
-  describe("POST /register", () => {
-    const request = async (body) =>
-      chai.request(app).post(`/register`).send(body);
+  afterEach(() => {
+    sinon.restore();
+  });
 
-    afterEach(() => {
-      sinon.restore();
-    });
+  describe("POST /", () => {
+    it("should create a new user", async () => {
+      const name = "name";
+      const email = "user@example.com";
+      const password = "123456";
+      const recaptchaValue = "validRecaptchaValue";
+      const googleResponse = { data: { success: true } };
 
-    it("should register a new user", async () => {
-      const user = {
-        name: "user",
-        email: "user1@example.com",
-        password: "123456",
-      };
+      sinon.stub(axios, "post").resolves(googleResponse);
+      const createStub = sinon
+        .stub(userService, "create")
+        .resolves("Usuário criado");
 
-      const hashedPassword = "e10adc3949ba59abbe56e057f20f883e";
-
-      sinon.stub(User, "create").returns(Promise.resolve());
-
-      const response = await request(user);
+      const response = await chai
+        .request(app)
+        .post("/register")
+        .send({ name, email, password, recaptchaValue });
 
       expect(response).to.have.status(201);
-      expect(User.create.firstCall.args[0]).to.deep.equal({
-        name: user.name,
-        email: user.email,
-        password: hashedPassword,
-      });
       expect(response.body.message).to.equal("Usuário criado");
+      expect(createStub.calledOnce).to.be.true;
     });
 
-    it("should throw an error if email is already registered", async () => {
-      const user = {
-        name: "user",
-        email: "user1@example.com",
-        password: "123456",
-      };
+    it("should handle error invalid name", async () => {
+      const name = "U";
+      const email = "user@email.com";
+      const password = "123456";
+      const recaptchaValue = "validRecaptchaValue";
+      const googleResponse = { data: { success: true } };
 
-      sinon.stub(User, "findOne").resolves({ email: user.email });
+      sinon.stub(axios, "post").resolves(googleResponse);
 
-      const response = await request(user);
-
-      expect(response).to.have.status(409);
-      expect(response.body.message).to.equal("Email já registrado");
-    });
-
-    it("should throw an error if an error occurs while creating the user", async () => {
-      const user = {
-        name: "user",
-        email: "user1@example.com",
-        password: "123456",
-      };
-
-      sinon.stub(User, "create").throws(new Error());
-
-      const response = await request(user);
-
-      expect(response).to.have.status(500);
-      expect(response).to.have.property("error");
-    });
-
-    it("should return an error for invalid name", async () => {
-      const user = {
-        name: "u",
-        email: "user1@example.com",
-        password: "123456",
-      };
-
-      const response = await request(user);
+      const response = await chai
+        .request(app)
+        .post("/register")
+        .send({ name, email, password, recaptchaValue });
 
       expect(response).to.have.status(401);
       expect(response.body.message).to.equal(
@@ -84,27 +57,37 @@ describe("Register Router", () => {
       );
     });
 
-    it("should return an error for invalid email", async () => {
-      const user = {
-        name: "user",
-        email: "invalidemail",
-        password: "123456",
-      };
+    it("should handle error invalid email", async () => {
+      const name = "User";
+      const email = "invalid";
+      const password = "123456";
+      const recaptchaValue = "validRecaptchaValue";
+      const googleResponse = { data: { success: true } };
 
-      const response = await request(user);
+      sinon.stub(axios, "post").resolves(googleResponse);
+
+      const response = await chai
+        .request(app)
+        .post("/register")
+        .send({ name, email, password, recaptchaValue });
 
       expect(response).to.have.status(401);
       expect(response.body.message).to.equal("Insira um email válido");
     });
 
-    it("should return an error for invalid password", async () => {
-      const user = {
-        name: "user",
-        email: "user1@example.com",
-        password: "123",
-      };
+    it("should handle error invalid password", async () => {
+      const name = "User";
+      const email = "user@example.com";
+      const password = "123";
+      const recaptchaValue = "validRecaptchaValue";
+      const googleResponse = { data: { success: true } };
 
-      const response = await request(user);
+      sinon.stub(axios, "post").resolves(googleResponse);
+
+      const response = await chai
+        .request(app)
+        .post("/register")
+        .send({ name, email, password, recaptchaValue });
 
       expect(response).to.have.status(401);
       expect(response.body.message).to.equal(
