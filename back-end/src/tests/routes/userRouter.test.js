c9816f9c549f35a8560743d84ace49ee837836c4
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-13 10:05:57 -0300

    test(routes): put /edit/email in userRouter in the backend

--- back-end/src/tests/routes/userRouter.test.js
+++ back-end/src/tests/routes/userRouter.test.js
@@ -59,4 +59,28 @@ describe("User Router", () => {
       expect(updateNameStub.calledOnce).to.be.true;
     });
   });
+
+  describe("PUT /edit/email", () => {
+    it("should update user email", async () => {
+      const user = { id: 1, name: "User", email: "user@example.com" };
+      const newEmail = "user2@example.com";
+      const token = "token";
+
+      const verifyStub = sinon.stub(jwt, "verify").returns(user);
+      const updateEmailStub = sinon
+        .stub(userService, "updateEmail")
+        .resolves("Email atualizado com sucesso!");
+
+      const response = await chai
+        .request(app)
+        .put("/profile/edit/email")
+        .set("Authorization", `Bearer ${token}`)
+        .send(newEmail);
+
+      expect(response).to.have.status(200);
+      expect(response.body.message).to.equal("Email atualizado com sucesso!");
+      expect(verifyStub.calledOnce).to.be.true;
+      expect(updateEmailStub.calledOnce).to.be.true;
+    });
+  });
 });
