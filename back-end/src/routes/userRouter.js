0e31f3c8b3b71b41baa907ab56d48b3bcf5e5bfb
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-13 12:00:37 -0300

    feat(routes): validName, validEmail and validPassword in userRouter in the backend

--- back-end/src/routes/userRouter.js
+++ back-end/src/routes/userRouter.js
@@ -1,11 +1,21 @@
 const router = require("express").Router();
 const userController = require("../controllers/userController");
-const { validToken } = require("../middlewares/createMiddleware");
+const {
+  validToken,
+  validName,
+  validEmail,
+  validPassword,
+} = require("../middlewares/createMiddleware");
 
 router.get("/", validToken, userController.getProfileInfo);
-router.put("/edit/name", validToken, userController.updateName);
-router.put("/edit/email", validToken, userController.updateEmail);
-router.put("/edit/password", validToken, userController.updatePassword);
+router.put("/edit/name", validToken, validName, userController.updateName);
+router.put("/edit/email", validToken, validEmail, userController.updateEmail);
+router.put(
+  "/edit/password",
+  validToken,
+  validPassword,
+  userController.updatePassword
+);
 router.delete("/", validToken, userController.deleteUser);
 router.post("/validate/email", userController.validateEmail);
 
