f29d23c1d19dafda6f7762d6219b242ea74be53c
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-13 10:40:14 -0300

    test(routes): post / in loginRouter in the backend

--- back-end/src/tests/routes/loginRouter.test.js
+++ back-end/src/tests/routes/loginRouter.test.js
@@ -4,58 +4,42 @@ const chaiHttp = require("chai-http");
 const sinon = require("sinon");
 const app = require("../../../index");
 const userService = require("../../services/userService");
+const axios = require("axios");
 
 const { expect } = chai;
 chai.use(chaiHttp);
 
 describe("Login Router", () => {
-  describe("POST /login", () => {
-    const request = async (body) => chai.request(app).post(`/login`).send(body);
+  afterEach(() => {
+    sinon.restore();
+  });
 
-    afterEach(() => {
-      sinon.restore();
-    });
-
-    it("should log in user and return a token when valid credentials", async () => {
+  describe("POST /", () => {
+    it("should log in a user", async () => {
       const user = {
-        email: "user1@example.com",
-        password: "123456",
-      };
-
-      sinon.stub(userService, "login").resolves({
         id: 1,
-        name: "user",
-        email: "user1@example.com",
+        name: "User",
+        email: "user@example.com",
         token: "token",
-      });
+      };
+      const email = "user@example.com";
+      const password = "123456";
+      const recaptchaValue = "validRecaptchaValue";
+      const googleResponse = { data: { success: true } };
 
-      const response = await request(user);
+      sinon.stub(axios, "post").resolves(googleResponse);
+      const loginStub = sinon.stub(userService, "login").resolves(user);
+
+      const response = await chai
+        .request(app)
+        .post("/login")
+        .send({ email, password, recaptchaValue });
 
       expect(response).to.have.status(200);
-      expect(response.body).to.have.property("id").to.equal(1);
-      expect(response.body).to.have.property("name").to.equal("user");
-      expect(response.body)
-        .to.have.property("email")
-        .to.equal("user1@example.com");
-      expect(response.body).to.have.property("token").to.be.a("string");
-    });
-
-    it("should return an error when invalid credentials", async () => {
-      const user = {
-        email: "user1@example.com",
-        password: "invalidpassword",
-      };
-
-      sinon
-        .stub(userService, "login")
-        .rejects({ statusCode: 404, message: "Email ou senha inválida" });
-
-      const response = await request(user);
-
-      expect(response).to.have.status(404);
-      expect(response.body)
-        .to.have.property("message")
-        .to.equal("Email ou senha inválida");
+      expect(response.body.name).to.equal(user.name);
+      expect(response.body.email).to.equal(user.email);
+      expect(response.body.token).to.equal(user.token);
+      expect(loginStub.calledOnce).to.be.true;
     });
   });
 });
