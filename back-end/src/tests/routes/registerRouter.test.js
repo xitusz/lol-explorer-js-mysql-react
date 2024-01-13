165804ecdd73c0b0c163ec0e79ad6fe216892fb6
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-13 12:52:05 -0300

    test(routes): assertions for firstCall in registerRouter in the backend

--- back-end/src/tests/routes/registerRouter.test.js
+++ back-end/src/tests/routes/registerRouter.test.js
@@ -35,6 +35,10 @@ describe("Register Router", () => {
       expect(response).to.have.status(201);
       expect(response.body.message).to.equal("Usuário criado");
       expect(createStub.calledOnce).to.be.true;
+      expect(createStub.firstCall.args[0]).to.equal(name);
+      expect(createStub.firstCall.args[1]).to.equal(email);
+      expect(createStub.firstCall.args[2]).to.equal(password);
+      expect(createStub.firstCall.args[3]).to.equal(recaptchaValue);
     });
   });
 });
