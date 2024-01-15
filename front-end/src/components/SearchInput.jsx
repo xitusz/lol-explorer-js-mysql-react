ac8e690d7b3be9cddac8cad878b5195f2b79f93a
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-15 19:11:57 -0300

    feat(components): SearchInput in the frontend

---
+++ front-end/src/components/SearchInput.jsx
@@ -0,0 +1,33 @@
+import React from "react";
+import PropTypes from "prop-types";
+import { AiOutlineSearch } from "react-icons/ai";
+
+const SearchInput = ({ iconSize, placeholder, value, onChange }) => {
+  return (
+    <div className="input-group mb-4 rounded-1">
+      <span className="input-group-text search-input border-0 text-white p-2 px-3">
+        <AiOutlineSearch size={iconSize} />
+      </span>
+      <input
+        type="text"
+        className="form-control search-input text-white border-0 p-0"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+      />
+    </div>
+  );
+};
+
+SearchInput.propTypes = {
+  iconSize: PropTypes.number,
+  placeholder: PropTypes.string.isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+};
+
+SearchInput.defaultProps = {
+  iconSize: 23,
+};
+
+export default SearchInput;
