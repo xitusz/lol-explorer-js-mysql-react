f864682c58c2d1bacafc697a8e7a1e8bd516790c
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-16 11:50:50 -0300

    chore(): remove import ProfileEdit css in App in the frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -2,7 +2,6 @@ import React from "react";
 import Routes from "./routes";
 import "./css/Character.css";
 import "./css/CharacterDetails.css";
-import "./css/ProfileEdit.css";
 import "./css/Region.css";
 
 const App = () => {
