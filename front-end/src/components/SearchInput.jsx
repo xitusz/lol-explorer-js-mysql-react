15515b3856ec62db4a3028cf0c99fca09230243c
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-16 11:38:07 -0300

    chore(components): import SearchInput css in SearchInput in the frontend

--- front-end/src/components/SearchInput.jsx
+++ front-end/src/components/SearchInput.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import { AiOutlineSearch } from "react-icons/ai";
+import "../css/SearchInput.css";
 
 const SearchInput = ({ iconSize, placeholder, value, onChange }) => {
   return (
