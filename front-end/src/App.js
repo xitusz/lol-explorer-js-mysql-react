63c33e254157ad9e0c5648ed2579654d9d1d45e4
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-16 11:34:19 -0300

    chore(): remove import Footer css in App in the frontend

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,7 +1,6 @@
 import React from "react";
 import Routes from "./routes";
 import "./css/Header.css";
-import "./css/Footer.css";
 import "./css/Character.css";
 import "./css/CharacterDetails.css";
 import "./css/Login.css";
