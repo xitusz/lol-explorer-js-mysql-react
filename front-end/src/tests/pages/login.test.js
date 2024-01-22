f3c4a940922a30c24de6c6f0f9c03b61afc2bc6d
G-Alves1 <2kgabrielalves@gmail.com>
2024-01-22 18:03:50 -0300

    test(pages): login in the frontend

--- front-end/src/tests/pages/login.test.js
+++ front-end/src/tests/pages/login.test.js
@@ -4,18 +4,27 @@ import { BrowserRouter } from "react-router-dom";
 import { render, fireEvent, screen } from "@testing-library/react";
 import Login from "../../pages/Login";
 
+jest.mock("../../context/AuthContext", () => ({
+  useAuth: () => ({
+    setUserToken: jest.fn(),
+  }),
+}));
+
 describe("Login page", () => {
   beforeEach(() => {
-    localStorage.clear();
-  });
-
-  it("should render the form correctly", () => {
     render(
       <BrowserRouter>
         <Login />
       </BrowserRouter>
     );
+  });
 
+  afterEach(() => {
+    jest.clearAllMocks();
+    localStorage.clear();
+  });
+
+  it("should render the form correctly", () => {
     expect(
       screen.getByRole("heading", { name: /conectar-se/i })
     ).toBeInTheDocument();
@@ -23,6 +32,7 @@ describe("Login page", () => {
       screen.getByPlaceholderText(/email@example.com/i)
     ).toBeInTheDocument();
     expect(screen.getByPlaceholderText("*********")).toBeInTheDocument();
+    expect(screen.getByTestId("recaptcha-login")).toBeInTheDocument();
     expect(screen.getByRole("button", { name: /entrar/i })).toBeInTheDocument();
     expect(
       screen.getByRole("button", { name: /cadastre-se/i })
@@ -30,12 +40,6 @@ describe("Login page", () => {
   });
 
   it("should update the email and password fields correctly", () => {
-    render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>
-    );
-
     const emailInput = screen.getByPlaceholderText(/email@example.com/i);
     const passwordInput = screen.getByPlaceholderText("*********");
 
@@ -46,93 +50,35 @@ describe("Login page", () => {
     expect(passwordInput.value).toBe("password");
   });
 
-  it("should set item in local storage with user email and password when Login button is clicked and redirect to home page", async () => {
-    const user = {
-      name: "name",
-      email: "email@example.com",
-      password: "password",
-      favorites: [],
-    };
+  // it("should handle user login correctly", () => {});
 
-    localStorage.setItem("userData", JSON.stringify([user]));
+  // it("should throw an error when email or password is invalid", () => {});
 
-    render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>
-    );
-
+  it("should throw an error when recaptcha is not checked", () => {
     const emailInput = screen.getByPlaceholderText(/email@example.com/i);
     const passwordInput = screen.getByPlaceholderText("*********");
     const loginButton = screen.getByRole("button", { name: /entrar/i });
 
-    fireEvent.change(emailInput, { target: { value: user.email } });
-    fireEvent.change(passwordInput, { target: { value: user.password } });
+    fireEvent.change(emailInput, { target: { value: "email@example.com" } });
+    fireEvent.change(passwordInput, { target: { value: "password" } });
     fireEvent.click(loginButton);
 
-    expect(localStorage.getItem("isLoggedIn")).toBe("true");
-    expect(JSON.parse(localStorage.getItem("user"))[0]).toEqual({
-      name: user.name,
-      email: user.email,
-      favorites: [],
-    });
-    expect(JSON.parse(localStorage.getItem("user"))).not.toBe(user.password);
-    expect(window.location.pathname).toBe("/");
-  });
-
-  it("should throw an error when email or password is invalid", async () => {
-    const user = {
-      name: "name",
-      email: "email@example.com",
-      password: "password",
-    };
-
-    localStorage.setItem("userData", JSON.stringify([user]));
-
-    render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>
-    );
-
-    const emailInput = screen.getByPlaceholderText(/email@example.com/i);
-    const passwordInput = screen.getByPlaceholderText("*********");
-    const loginButton = screen.getByRole("button", { name: /entrar/i });
-
-    fireEvent.change(emailInput, {
-      target: { value: "wrongemail@example.com" },
-    });
-    fireEvent.change(passwordInput, { target: { value: user.password } });
-    fireEvent.click(loginButton);
-
-    expect(screen.getByText("Email ou senha inválida")).toBeInTheDocument();
+    expect(screen.getByText("Captcha inválido")).toBeInTheDocument();
     expect(localStorage.getItem("isLoggedIn")).toBeNull();
-    expect(localStorage.getItem("user")).toBeNull();
   });
 
   it("should redirect to home page if user is already logged in", () => {
     localStorage.setItem("isLoggedIn", true);
 
-    render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>
-    );
-
-    expect(localStorage.getItem("isLoggedIn")).toBeTruthy();
+    expect(localStorage.getItem("isLoggedIn")).toBe("true");
     expect(window.location.pathname).toBe("/");
   });
 
   it("should redirect to register page when register button is clicked", async () => {
-    render(
-      <BrowserRouter>
-        <Login />
-      </BrowserRouter>
-    );
-
     const registerButton = screen.getByRole("button", {
       name: /cadastre-se/i,
     });
+
     fireEvent.click(registerButton);
 
     expect(window.location.pathname).toBe("/register");
