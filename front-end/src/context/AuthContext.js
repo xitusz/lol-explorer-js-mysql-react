14d1d70399d3d3119d5241b4f2f0401a91fc0c2e
G-Alves1 <2kgabrielalves@gmail.com>
2024-05-15 16:52:07 -0300

    refactor(context): remove localStorage usage in AuthContext in the frontend

--- front-end/src/context/AuthContext.js
+++ front-end/src/context/AuthContext.js
@@ -1,20 +1,11 @@
 import React from "react";
-import { createContext, useContext, useState, useEffect } from "react";
+import { createContext, useContext, useState } from "react";
 import PropTypes from "prop-types";
-import { getItemFromLocalStorage } from "../services/localStorage";
 
 const AuthContext = createContext();
 
 export const AuthProvider = ({ children }) => {
   const [userToken, setUserToken] = useState("");
-
-  useEffect(() => {
-    const token = getItemFromLocalStorage("token");
-
-    if (token) {
-      setUserToken(token);
-    }
-  }, []);
 
   return (
     <AuthContext.Provider value={{ userToken, setUserToken }}>
